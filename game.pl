7cdd73595a0be90b5b666474130143fed2e2317f
Adi Weinstock <adi.wein6@gmail.com>
2020-09-26 23:39:44 +0300

    Implemented the human player turn

--- game.pl
+++ game.pl
@@ -5,16 +5,18 @@
 
 % Computer player "thinks" and makes a move
 % TODO: method will use dfs to find move with best score, and call move(BestMove)
-computer_turn(Player):-
+computer_turn(Player, BoardState, NewBoardState):-
     % TODO: use bfs (/ dfs) to find best move
     % make_move(BestMove)
     !.
 
 % Player is asked to make a move, selected move is made
 human_player_turn(Player, BoardState, NewBoardState):-
-    pick_ball_to_move(BoardState, white, Row, Column),
-    pick_possible_move(BoardState, white, Row, Column, Direction),
-    move(BoardState, Player, Row, Column, Direction, NewBoardState).
+    pick_ball_to_move(BoardState, Player, Row, Column),
+    pick_possible_move(BoardState, Player, Row, Column, Direction),
+    move(BoardState, Player, Row, Column, Direction, NewBoardState),
+    other_player(Player, OtherPlayer),
+    computer_turn(OtherPlayer, NewBoardState, _).
 
 
 % Repeatedly plays turns of players (human, computer, human...) until someone wins
@@ -24,8 +26,4 @@ run_game():-
     pick_board_size(BoardSize),
     init_board(BoardSize, BoardState),
     display_board(BoardSize, BoardState),
-    repeat,
-    (
-        human_player_turn(white),
-        computer_turn(black)
-    ).
+    human_player_turn(white, BoardState, _).
